56a60355d8667528f579b8d82f2029b3b2ec6f22
Niharika Balachandra <niharikabalachandra@gmail.com>
2018-02-02 11:32:52 -0600

    Add files via upload

---
+++ App/js/app.js
@@ -0,0 +1,181 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+console.log('hi');
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
+
+
+(function(){ 
+  // construct the d3 container
+  var d3 = window.d3;
+  var format = d3.format(".0f");
+
+  var line = d3.svg.line();
+  var width = 960,
+      height = 960;
+
+  var svg = d3.select("#container").append("svg")
+    .attr('id', 'maps')
+    .attr("viewBox", "0 0 1680 840");
+
+  var usa = svg.append('svg:image')
+    .attr("xlink:href","images/usa_BuPu.svg")
+    .attr('id', 'usa')
+    .attr('class', 'map')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('x', 300)
+
+  var ohio = svg.append('svg:image')
+    .attr("xlink:href","images/oh_BuPu.svg")
+    .attr('id', 'ohio')
+    .attr('class', 'map')
+    .attr('width', 74)
+    .attr('x', 934)
+    .attr('y', 532)
+    .attr('transform', 'rotate(-8 0 0)')
+    .on("click", clicked);
+
+
+
+function clicked(d) {
+
+  var map = svg.selectAll("map");
+
+  svg.transition()
+    .duration(1000)
+    .attr("transform", "translate(-2200,20)scale(12)")
+    .ease('easeIn')
+}
+
+
+})();
+
