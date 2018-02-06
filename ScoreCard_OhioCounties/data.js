97f9c9aa11519817cbf975224551e04aa19caf70
Niharika Balachandra <niharikabalachandra@gmail.com>
2018-02-06 14:44:55 -0600

    Add files via upload

---
+++ ScoreCard_OhioCounties/data.js
@@ -0,0 +1,55 @@
+var json = [
+  {
+    "name": "Auglaize",
+    "value": 9,
+    "category": "B"
+  },
+    {
+    "name": "Delaware",
+    "value": 1,
+    "category": "A"
+  },
+  {
+    "name": "Geauga",
+    "value": 2,
+    "category": "A"
+  },
+  {
+    "name": "Holmes",
+    "value": 7,
+    "category": "B"
+  },
+  {
+    "name": "Medina",
+    "value": 5,
+    "category": "A"
+  },
+  {
+    "name": "Mercer",
+    "value": 10,
+    "category": "B"
+  },
+  {
+    "name": "Putnam",
+    "value": 3,
+    "category": "A"
+  },
+  {
+    "name": "Union",
+    "value": 4,
+    "category": "A"
+  },
+  {
+    "name": "Warren",
+    "value": 6,
+    "category": "B"
+  },
+  {
+    "name": "Wood",
+    "value": 8,
+    "category": "B"
+  },
+  
+  
+  
+];
---
+++ ScoreCard_OhioCounties/scripts.js
@@ -0,0 +1,44 @@
+json.sort(function(a,b){
+  return b.value - a.value;
+});
+
+var dataArray = [];
+var headers = ['Name', 'Value', {role: 'style'}, {role: 'annotation'}]
+dataArray.push(headers);
+json.forEach(function(d,i){
+  d.category === 'A' ? fill = '#CCC' : fill =  '#B44682';
+  i === 0 || i === json.length - 1 ? annotation = d.value : annotation = null;
+  dataArray.push([d.name, d.value, fill, annotation]);
+});
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable(dataArray);
+
+  var options = {
+    chartArea: {
+      top: 10,
+      bottom: 0,
+      left: 65,
+      right: 50
+    },
+    hAxis: {
+      gridlines: {
+        color: '#fff'
+      }
+    },
+    legend: 'none',
+    vAxis: {
+        textStyle: {
+        fontName: 'Helvetica Neue',
+        fontSize: 14
+      }
+    }
+  };
+
+  var chart = new google.visualization.BarChart(document.getElementById('chart'));
+
+  chart.draw(data, options);
+}
