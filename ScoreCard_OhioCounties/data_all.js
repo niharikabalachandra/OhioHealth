15b86814c3ebe1d22e7226d9ffe9ca5d1a00a8dd
Niharika Balachandra <niharikabalachandra@gmail.com>
2018-02-06 18:11:02 -0600

    Add files via upload

---
+++ ScoreCard_OhioCounties/data_all.js
@@ -0,0 +1,444 @@
+var json = [
+  {
+    "name": "Adams",
+    "value": 87,
+    "category": "A"
+  },
+  {
+    "name": "Allen",
+    "value": 46,
+    "category": "A"
+  },
+  {
+    "name": "Ashland",
+    "value": 19,
+    "category": "A"
+  },
+  {
+    "name": "Ashtabula",
+    "value": 75,
+    "category": "A"
+  },
+  {
+    "name": "Athens",
+    "value": 72,
+    "category": "A"
+  },
+  {
+    "name": "Auglaize",
+    "value": 9,
+    "category": "B"
+  },
+  {
+    "name": "Belmont",
+    "value": 56,
+    "category": "A"
+  },
+  {
+    "name": "Brown",
+    "value": 74,
+    "category": "A"
+  },
+  {
+    "name": "Butler",
+    "value": 48,
+    "category": "A"
+  },
+  {
+    "name": "Carroll",
+    "value": 47,
+    "category": "A"
+  },
+  {
+    "name": "Champaign",
+    "value": 35,
+    "category": "A"
+  },
+  {
+    "name": "Clark",
+    "value": 73,
+    "category": "A"
+  },
+  {
+    "name": "Clermont",
+    "value": 40,
+    "category": "A"
+  },
+  {
+    "name": "Clinton",
+    "value": 62,
+    "category": "A"
+  },
+  {
+    "name": "Columbiana",
+    "value": 55,
+    "category": "A"
+  },
+  {
+    "name": "Coshocton",
+    "value": 60,
+    "category": "A"
+  },
+  {
+    "name": "Crawford",
+    "value": 44,
+    "category": "A"
+  },
+  {
+    "name": "Cuyahoga",
+    "value": 65,
+    "category": "A"
+  },
+  {
+    "name": "Darke",
+    "value": 32,
+    "category": "A"
+  },
+  {
+    "name": "Defiance",
+    "value": 20,
+    "category": "A"
+  },
+  {
+    "name": "Delaware",
+    "value": 1,
+    "category": "A"
+  },
+  {
+    "name": "Erie",
+    "value": 57,
+    "category": "A"
+  },
+  {
+    "name": "Fairfield",
+    "value": 15,
+    "category": "A"
+  },
+  {
+    "name": "Fayette",
+    "value": 76,
+    "category": "A"
+  },
+  {
+    "name": "Franklin",
+    "value": 49,
+    "category": "A"
+  },
+  {
+    "name": "Fulton",
+    "value": 29,
+    "category": "A"
+  },
+  {
+    "name": "Gallia",
+    "value": 85,
+    "category": "A"
+  },
+  {
+    "name": "Geauga",
+    "value": 2,
+    "category": "A"
+  },
+  {
+    "name": "Greene",
+    "value": 13,
+    "category": "A"
+  },
+  {
+    "name": "Guernsey",
+    "value": 70,
+    "category": "A"
+  },
+  {
+    "name": "Hamilton",
+    "value": 58,
+    "category": "A"
+  },
+  {
+    "name": "Hancock",
+    "value": 14,
+    "category": "A"
+  },
+  {
+    "name": "Hardin",
+    "value": 64,
+    "category": "A"
+  },
+  {
+    "name": "Harrison",
+    "value": 67,
+    "category": "A"
+  },
+  {
+    "name": "Henry",
+    "value": 11,
+    "category": "A"
+  },
+  {
+    "name": "Highland",
+    "value": 80,
+    "category": "A"
+  },
+  {
+    "name": "Hocking",
+    "value": 59,
+    "category": "A"
+  },
+  {
+    "name": "Holmes",
+    "value": 7,
+    "category": "A"
+  },
+  {
+    "name": "Huron",
+    "value": 38,
+    "category": "A"
+  },
+  {
+    "name": "Jackson",
+    "value": 84,
+    "category": "A"
+  },
+  {
+    "name": "Jefferson",
+    "value": 79,
+    "category": "A"
+  },
+  {
+    "name": "Knox",
+    "value": 34,
+    "category": "A"
+  },
+  {
+    "name": "Lake",
+    "value": 23,
+    "category": "A"
+  },
+  {
+    "name": "Lawrence",
+    "value": 82,
+    "category": "A"
+  },
+  {
+    "name": "Licking",
+    "value": 31,
+    "category": "A"
+  },
+  {
+    "name": "Logan",
+    "value": 43,
+    "category": "A"
+  },
+  {
+    "name": "Lorain",
+    "value": 41,
+    "category": "A"
+  },
+  {
+    "name": "Lucas",
+    "value": 69,
+    "category": "A"
+  },
+  {
+    "name": "Madison",
+    "value": 25,
+    "category": "A"
+  },
+  {
+    "name": "Mahoning",
+    "value": 71,
+    "category": "A"
+  },
+  {
+    "name": "Marion",
+    "value": 68,
+    "category": "A"
+  },
+  {
+    "name": "Medina",
+    "value": 5,
+    "category": "B"
+  },
+  {
+    "name": "Meigs",
+    "value": 66,
+    "category": "A"
+  },
+  {
+    "name": "Mercer",
+    "value": 10,
+    "category": "B"
+  },
+  {
+    "name": "Miami",
+    "value": 26,
+    "category": "A"
+  },
+  {
+    "name": "Monroe",
+    "value": 39,
+    "category": "A"
+  },
+  {
+    "name": "Montgomery",
+    "value": 77,
+    "category": "A"
+  },
+  {
+    "name": "Morgan",
+    "value": 78,
+    "category": "A"
+  },
+  {
+    "name": "Morrow",
+    "value":30,
+    "category": "A"
+  },
+  {
+    "name": "Muskingum",
+    "value": 63,
+    "category": "A"
+  },
+  {
+    "name": "Noble",
+    "value": 22,
+    "category": "A"
+  },
+  {
+    "name": "Ottawa",
+    "value": 16,
+    "category": "A"
+  },
+  {
+    "name": "Paulding",
+    "value": 17,
+    "category": "A"
+  },
+  {
+    "name": "Perry",
+    "value": 52,
+    "category": "A"
+  },
+  {
+    "name": "Pickaway",
+    "value": 42,
+    "category": "A"
+  },
+  {
+    "name": "Pike",
+    "value": 88,
+    "category": "A"
+  },
+  {
+    "name": "Portage",
+    "value": 27,
+    "category": "A"
+  },
+  {
+    "name": "Preble",
+    "value": 51,
+    "category": "A"
+  },
+  {
+    "name": "Putnam",
+    "value": 3,
+    "category": "B"
+  },
+  {
+    "name": "Richland",
+    "value": 54,
+    "category": "A"
+  },
+  {
+    "name": "Ross",
+    "value": 81,
+    "category": "A"
+  },
+  {
+    "name": "Sandusky",
+    "value": 33,
+    "category": "A"
+  },
+  {
+    "name": "Scioto",
+    "value": 86,
+    "category": "A"
+  },
+  {
+    "name": "Seneca",
+    "value": 53,
+    "category": "A"
+  },
+  {
+    "name": "Shelby",
+    "value": 21,
+    "category": "A"
+  },
+  {
+    "name": "Stark",
+    "value": 45,
+    "category": "A"
+  },
+  {
+    "name": "Summit",
+    "value": 50,
+    "category": "A"
+  },
+  {
+    "name": "Trumbull",
+    "value": 61,
+    "category": "A"
+  },
+  {
+    "name": "Tuscarawas",
+    "value": 36,
+    "category": "A"
+  },
+  {
+    "name": "Union",
+    "value": 4,
+    "category": "B"
+  },
+  {
+    "name": "Van Wert",
+    "value": 24,
+    "category": "A"
+  },
+  {
+    "name": "Vinton",
+    "value": 83,
+    "category": "A"
+  },
+  {
+    "name": "Warren",
+    "value": 6,
+    "category": "B"
+  },
+  {
+    "name": "Washington",
+    "value": 37,
+    "category": "A"
+  },
+  {
+    "name": "Wayne",
+    "value": 12,
+    "category": "A"
+  },
+  {
+    "name": "Williams",
+    "value": 18,
+    "category": "A"
+  },
+  {
+    "name": "Wood",
+    "value": 8,
+    "category": "B"
+  },
+  {
+    "name": "Wyandot",
+    "value": 28,
+    "category": "A"
+  },
+  
+  
+];
